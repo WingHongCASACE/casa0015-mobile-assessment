a65ba9d2b57d3b73973a9370782b1f01b9e4752e
WingHongCASACE <ucfnwho@ucl.ac.uk>
2022-03-01 17:42:13 +0000

    fix permission, able to show coordinates in console

--- test/testFunction.dart
+++ test/testFunction.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 
 void main() => runApp(MyApp());
 
@@ -17,12 +18,26 @@ class TestLocation extends StatefulWidget {
 }
 
 class _TestLocationState extends State<TestLocation> {
+  // double Lat;
+  // double Long;
+
+  void getCoordinate() async {
+    LocationPermission permission = await Geolocator.requestPermission();
+    if (permission == LocationPermission.denied) {
+      return Future.error('Location permissions are denied');
+    }
+    Position position = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.high);
+    print(position);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         body: Center(
       child: ElevatedButton(
         onPressed: () {
+          getCoordinate();
           print('recorded coordinate');
         },
         child: const Text('get coordinate'),
